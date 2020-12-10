3b72298e20fca9fdea1869973eb11d56cb98a388
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2020-12-10 21:55:17 +0100

    Add more glyphs and experiment with sound

--- jbox-lib/test-game.js
+++ jbox-lib/test-game.js
@@ -11,7 +11,7 @@ jb.init({
     //     x / (i / 10) + (i / 2) * Math.sin(i),
     //     y / (i / 10) + (i / 2) * Math.cos(i)
     //   );
-    jb.print("0 123456789", 5, 10, 0);
+    jb.print('it\'s on! "DUPA"%&+-*/:;_', 5, 10, 0);
   },
 
   update: function () {
@@ -29,8 +29,30 @@ jb.init({
     if (jb.btn(jb.BTN_DOWN)) {
       y++;
     }
+
+    if (jb.btnp(jb.BTN_A)) {
+      soundEffect(500, 0, 0.5, "sawtooth", 0.5, 0, 0, 1000, false, 0);
+    }
   },
 });
+
+// var bufferSize = 4096;
+// var brownNoise = (function () {
+//   var lastOut = 0.0;
+//   var node = actx.createScriptProcessor(bufferSize, 1, 1);
+//   node.onaudioprocess = function (e) {
+//     var output = e.outputBuffer.getChannelData(0);
+//     for (var i = 0; i < bufferSize; i++) {
+//       var white = Math.random() * 2 - 1;
+//       output[i] = (lastOut + 0.02 * white) / 1.02;
+//       lastOut = output[i];
+//       output[i] *= 3.5; // (roughly) compensate for gain
+//     }
+//   };
+//   return node;
+// })();
+
+// brownNoise.connect(actx.destination);
 
 // // jb.map();
 // console.log(data.sprites.slice(64 * 8, 64 * 26));
