fd892c75e68ebf384ab3807ec3518b7984d590b4
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2024-01-24 12:38:57 +0100

    Fix menus taking lots of space

---
+++ sparky/game/game.js
@@ -0,0 +1,6 @@
+if (
+  localStorage.getItem(LOCAL_STORAGE_CODE_KEY) &&
+  localStorage.getItem(LOCAL_STORAGE_DATA_KEY)
+) {
+  eval(localStorage.getItem("SPARKY_GAME_DATA"));
+  eval(localStorage.getItem(LOCAL_STORAGE_DATA_KEY)}
